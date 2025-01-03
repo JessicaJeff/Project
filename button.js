d2a7532667273394a10e6834036e50ec2d1b5ff0
Jessica Jeffery <jessicajeffery@Jessicas-MacBook-Air-2.local>
2025-01-03 20:55:36 +0000

    Update button.js, index.html, styles.css; Add new photos; Remove old photo

--- button.js
+++ button.js
@@ -16,3 +16,17 @@ function toggleColor() {
 
 // Attach the function to the button's click event
 fireButton.addEventListener("click", toggleColor);
+
+// Image function
+const lauraPhoto = document.getElementById("photo-laura");
+// Change the laura palma image to dale cooper when clicked 
+lauraPhoto.addEventListener("click", () => {
+  if (lauraPhoto.src.includes("Photo1.webp")) {
+    lauraPhoto.src = "./Public/Photo2.jpeg";
+    lauraPhoto.alt = "Dale Cooper";
+  } else {
+    // Change back to Laura Palmer image
+    lauraPhoto.src = "./Public/Photo1.webp";
+    lauraPhoto.alt = "Laura Palmer";
+  }
+});
